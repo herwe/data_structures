dd39a95dbc3ac081a5c3b04186371efe43ddecd3
Herman Westerman <herman.westerman@gmail.com>
2022-10-13 17:59:21 +0200

    add skeleton

---
+++ src/stack/MyStack.java
@@ -0,0 +1,24 @@
+package stack;
+
+public class MyStack<T> implements MyStackInterface<T> {
+
+    @Override
+    public T push() {
+        return null;
+    }
+
+    @Override
+    public T pop() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/stack/MyStackInterface.java
@@ -0,0 +1,8 @@
+package stack;
+
+public interface MyStackInterface<T> {
+    public T push();
+    public T pop();
+    public T peek();
+    public boolean isEmpty();
+}
